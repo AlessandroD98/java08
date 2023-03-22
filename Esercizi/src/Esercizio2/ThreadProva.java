4cfac6d89d322fe5a9fd486539611c1ca631465e
AlessandroD98 <alessandro.comunicazioni@gmali.com>
2023-03-22 17:31:22 +0100

    c

--- Esercizi/src/Esercizio2/ThreadProva.java
+++ Esercizi/src/Esercizio2/ThreadProva.java
@@ -14,7 +14,6 @@ public class ThreadProva extends Thread{
 	private int start;
 	private int end;
 	private int[] arr;
-	//private List<Integer> arr;
 	
 	public ThreadProva (String nome, int start, int end) {
 		this.nome = nome;
@@ -26,25 +25,21 @@ public class ThreadProva extends Thread{
 	public void CreaArray() {
 		
 	this.arr = new int[3000];
-		//this.arr = new ArrayList<Integer>();
 		
 		for(int i = 0; i < 3000; i++) {
-			int b =(int)( Math.random() * 100) + 1;
-			arr[i] = b;
-			//arr.add(b);
+			arr[i] = (int)( Math.random() * 100) + 1;
+			
 		}
 	}
 	
 	public void run() {
 		  
-		//Optional<Integer> sum = this.arr.stream().reduce((element1, element2) -> element1 + element2);
 		int[] array = arraySlice();
 		List<Integer> finalArray = new ArrayList<Integer>();
 		
 		for(int i = 0; i < array.length; i++) {
 			finalArray.add(array[i]);
 		}
-		
 		
 		int sum = finalArray.stream().reduce((element1, element2) -> element1 + element2).orElse(0);
 		System.out.println("Risultato parziale di " + this.nome + ": " + sum);
